5ed5f9a9fe47c6bed6fa9bf7b2b03659af59bb24
IGORAOLIV <igoralmeida0105@gmail.com>
2023-08-29 13:22:47 -0300

    aplicando funções ao desafio 3

--- 03 - lista de exercios/revisao-desafio-2.js
+++ 03 - lista de exercios/revisao-desafio-2.js
@@ -12,19 +12,27 @@ IMC em adultos Condição:
 - Entre 30 e 40 Obeso;
 - Acima de 40 Obsesidade Grave; */
 
-const altura = 1.54;
-const peso = 64;
-
-const imc = peso / Math.pow(altura, 2);
-
-if (imc < 18.5) {
-  console.log("IMC: " + imc.toFixed(1) + " - Abaixo do peso.");
-} else if (imc >= 18.5 && imc <= 25) {
-  console.log("IMC: " + imc.toFixed(1) + " - Peso normal.");
-} else if (imc >= 25 && imc <= 30) {
-  console.log("IMC: " + imc.toFixed(1) + " - Acima do peso.");
-} else if (imc >= 30 && imc <= 40) {
-  console.log("IMC: " + imc.toFixed(1) + " - Obesidade.");
-} else {
-  console.log("IMC: " + imc.toFixed(1) + " - Obesidade grave.");
+function caucularImc(peso, altura) {
+  return (imc = peso / Math.pow(altura, 2));
 }
+function classificarImc(imc) {
+  if (imc < 18.5) {
+    return "IMC: " + imc.toFixed(1) + " - Abaixo do peso.";
+  } else if (imc >= 18.5 && imc <= 25) {
+    return "IMC: " + imc.toFixed(1) + " - Peso normal.";
+  } else if (imc >= 25 && imc <= 30) {
+    return "IMC: " + imc.toFixed(1) + " - Acima do peso.";
+  } else if (imc >= 30 && imc <= 40) {
+    return "IMC: " + imc.toFixed(1) + " - Obesidade.";
+  } else {
+    return "IMC: " + imc.toFixed(1) + " - Obesidade grave.";
+  }
+}
+function main() {
+  const altura = 1.54;
+  const peso = 134;
+  const imc = caucularImc(peso, altura);
+  console.log(classificarImc(imc));
+}
+
+main();
