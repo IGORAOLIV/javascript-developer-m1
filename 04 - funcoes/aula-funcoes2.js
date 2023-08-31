138bb201e836f441e316be4bf392c082341eb136
IGORAOLIV <igoralmeida0105@gmail.com>
2023-08-31 11:27:12 -0300

    Revisa exercicios funções

---
+++ 04 - funcoes/aula-funcoes2.js
@@ -0,0 +1,15 @@
+function escreveMeuNome(nome) {
+  console.log("meu nome é " + nome);
+}
+
+escreveMeuNome("Igor");
+
+function verificaIdade(idade) {
+  if (idade >= 18) {
+    console.log("Sou maior de idade");
+  } else {
+    console.log("Sou menor de idade");
+  }
+}
+
+verificaIdade(36);
--- 04 - funcoes/juros-funcoes.js
+++ 04 - funcoes/juros-funcoes.js
@@ -6,33 +6,31 @@ Código Condição de pagamento:
 4 - Acima de duas vezes, preço normal de etiqueta mais Juros de 10%;
 */
 
-function aplicarDesconto(valor, desconto){
-    return (valor - (valor * (desconto / 100)));
+function aplicarDesconto(valor, desconto) {
+  return valor - valor * (desconto / 100);
 }
 
-function aplicaJuros(valor, juros){
-    return (valor + (valor * (juros / 100)));
+function aplicaJuros(valor, juros) {
+  return valor + valor * (juros / 100);
 }
 
 //Valor do precoEtiqueta
 let precoEtiqueta = 100; // substitua pelo valor do produto
 
 // Condições de pagamento
-let fomarPagamento = 2; // substitua pela forma de pagamento escolhida
+let fomarPagamento = 4; // substitua pela forma de pagamento escolhida
 
 // Calcula valor final
-if (fomarPagamento === 1){
-    valorTotal = (aplicarDesconto(precoEtiqueta, 10));
-} else if (fomarPagamento === 2){
-    valorTotal = (aplicarDesconto(precoEtiqueta, 15));
-} else if (fomarPagamento === 3){
-    valorTotal = precoEtiqueta;
-} else if (fomarPagamento === 4){
-    valorTotal = (aplicaJuros(precoEtiqueta, 10));
+if (fomarPagamento === 1) {
+  valorTotal = aplicarDesconto(precoEtiqueta, 10);
+} else if (fomarPagamento === 2) {
+  valorTotal = aplicarDesconto(precoEtiqueta, 15);
+} else if (fomarPagamento === 3) {
+  valorTotal = precoEtiqueta;
 } else {
-    console.log('Forma de pagamento inválida!');
+  valorTotal = aplicaJuros(precoEtiqueta, 10);
 }
 
 // Exibe valor final
-console.log('Valor do produto: R$ ' + precoEtiqueta.toFixed(2))
-console.log('Preço a ser pago: R$ ' + valorTotal.toFixed(2))
+console.log("Valor do produto: R$ " + precoEtiqueta.toFixed(2));
+console.log("Preço a ser pago: R$ " + valorTotal.toFixed(2));
